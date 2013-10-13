1ef0c845e8a72d3745843b868a0a2fe4e77633a9
Jim Foltz <jim.foltz@gmail.com>
2013-10-13 08:43:53 -0400

    Changed default encoding for imported files
     * Use Windows-1252 encoding
     * Enclose in JF namespace module
     * Remove debugging output
     * Add -p (pretty) to command line options
     * Default to output lines rather than pretty
     * Use less pager automatically

--- dxf2ruby.rb
+++ dxf2ruby.rb
@@ -1,19 +1,17 @@
 # dxf2ruby.rb - (C) 2011 jim.foltz@gmail.com
-# 
-#     This library is free software; you can redistribute it and/or
-#     modify it under the terms of the GNU Lesser General Public
-#     License as published by the Free Software Foundation; either
-#     version 2.1 of the License, or (at your option) any later version.
-# 
-#     This library is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#     Lesser General Public License for more details.
-# 
-#     You should have received a copy of the GNU Lesser General Public
-#     License along with this library; if not, write to the Free Software
-#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 # About DXF (Nots from AutoCAD)
 #
 # DXF _Objects_ have no graphical representation. (aka nongraphical objects)
@@ -25,136 +23,150 @@
 # in an entity. With each new AutoCAD release, new group codes will be added to
 # entities to accommodate additional features.
 
-# HISTORY
-# 2013-10-3 Version 1.0
-#  * Added LGPL License
-#  * Uploaded to https://github.com/jimfoltz/ruby-dxf-reader
-#
+module JF
+  module Dxf2Ruby
 
-module Dxf2Ruby
-
-  module_function
-
-  def parse(filename)
-    fp        = File.open(filename)
-    dxf       = {'HEADER' => {}, 'BLOCKS' => [], 'ENTITIES' => []}
-
-    #
-    # main loop
-    #
-
-    while true
-      c, v = read_codes(fp)
-      break if v == "EOF"
-      if v == "SECTION"
-        c, v = read_codes(fp)
-
-        if v == "HEADER"
-          hdr = dxf['HEADER']
-          while true
-            c, v = read_codes(fp)
-            break if v == "ENDSEC"
-            if c == 9
-              key = v
-              hdr[key] = {}
-            else
-              add_att(hdr[key], c, v)
-            end
-          end # while
-        end # if HEADER
-
-        if v == "BLOCKS"
-          blks = dxf[v]
-          parse_entities(blks, fp)
-        end # if BLOCKS
-
-        if v == "ENTITIES"
-          ents = dxf[v]
-          parse_entities(ents, fp)
-        end #  ENTITIES section
-
-      end # if in SECTION
-
-    end # main loop
-
-    return dxf
-  end
-
-  def parse_entities(section, fp)
-    last_ent = nil
-    last_code = nil
-    while true
-      c, v = read_codes(fp)
-      break if v == "ENDSEC"
-      next if c == 999
-      # LWPOLYLINE seems to break the rule that we can ignore the order of codes.
-      if last_ent == "LWPOLYLINE"
-        if c == 10
-          section[-1][42] ||= []
-          # Create default 42
-          add_att(section[-1], 42, 0.0)
-        end
-        if c == 42
-          # update default
-          section[-1][42][-1] = v
-          next
-        end
-      end
-      if c == 0
-        last_ent = v
-        section << {c => v}
+    def self.parse(filename)
+      if RUBY_VERSION == '2.0.0'
+        read_flag = 'r:Windows-1252:UTF-8'
       else
-        add_att(section[-1], c, v)
+        read_flag = 'r'
       end
-      last_code = c
-    end # while
-  end # def parse_entities
+      fp = File.open(filename, read_flag)
+      dxf = {'HEADER' => {}, 'BLOCKS' => [], 'ENTITIES' => []}
 
-  def read_codes(fp)
-    c = fp.gets.to_i
-    v = fp.gets.strip
-    v.upcase! if c == 0
-    case c
-    when 10..59, 140..147, 210..239, 1010..1059
-      v = v.to_f
-    when 60..79, 90..99, 170..175,280..289, 370..379, 380..389,500..409, 1060..1079
-      v = v.to_i
+      #
+      # main loop
+      #
+
+      while true
+        c, v = read_codes(fp)
+        break if v == "EOF"
+        if v == "SECTION"
+          c, v = read_codes(fp)
+
+          if v == "HEADER"
+            hdr = dxf['HEADER']
+            while true
+              c, v = read_codes(fp)
+              break if v == "ENDSEC" # or v == "BLOCKS" or v == "ENTITIES" or v == "EOF"
+              if c == 9
+                key = v
+                hdr[key] = {}
+              else
+                add_att(hdr[key], c, v)
+              end
+            end # while
+          end # if HEADER
+
+          if v == "BLOCKS"
+            blks = dxf[v]
+            parse_entities(blks, fp)
+          end # if BLOCKS
+
+          if v == "ENTITIES"
+            ents = dxf[v]
+            parse_entities(ents, fp)
+          end #  ENTITIES section
+
+        end # if in SECTION
+
+      end # main loop
+
+      fp.close
+      return dxf
     end
-    return( [c, v] )
-  end
 
-  def add_att(ent, code, value)
-    # Initially, I thought each code mapped to a single value. Turns out
-    # a code can be a list of values. 
-    if ent.nil? and $JFDEBUG
-      p caller
-      p code
-      p value
+    def self.parse_entities(section, fp)
+      last_ent = nil
+      last_code = nil
+      while true
+        c, v = read_codes(fp)
+        break if v == "ENDSEC" or v == "EOF"
+        next if c == 999
+        # LWPOLYLINE seems to break the rule that we can ignore the order of codes.
+        if last_ent == "LWPOLYLINE"
+          if c == 10
+            section[-1][42] ||= []
+            # Create default 42
+            add_att(section[-1], 42, 0.0)
+          end
+          if c == 42
+            # update default
+            section[-1][42][-1] = v
+            next
+          end
+        end
+        if c == 0
+          last_ent = v
+          section << {c => v}
+        else
+          add_att(section[-1], c, v)
+        end
+        last_code = c
+      end # while
+    end # def self.parse_entities
+
+    def self.read_codes(fp)
+      c = fp.gets
+      return [0, "EOF"] if c.nil?
+      v = fp.gets
+      return [0, "EOF"] if v.nil?
+      c = c.to_i
+      v.strip!
+      v.upcase! if c == 0
+      case c
+      when 10..59, 140..147, 210..239, 1010..1059
+        v = v.to_f
+      when 60..79, 90..99, 170..175,280..289, 370..379, 380..389,500..409, 1060..1079
+        v = v.to_i
+      end
+      return( [c, v] )
     end
-    if ent[code].nil?
-      ent[code] = value
-    elsif ent[code].class == Array
-      ent[code] << value
-    else
-      t = ent[code]
-      ent[code] = []
-      ent[code] << t
-      ent[code] << value
+
+    def self.add_att(ent, code, value)
+      # Initially, I thought each code mapped to a single value. Turns out
+      # a code can be a list of values. 
+      if ent.nil? and $JFDEBUG
+        p caller
+        p code
+        p value
+      end
+      if ent[code].nil?
+        ent[code] = value
+      elsif ent[code].class == Array
+        ent[code] << value
+      else
+        t = ent[code]
+        ent[code] = []
+        ent[code] << t
+        ent[code] << value
+      end
     end
-  end
 
 
-end # module Dxf2Ruby
+  end # mod Dxf2Ruby
+end # mod JF
 
 
 if $0 == __FILE__
-  require 'pp'
   t1 = Time.now
-  dxf = Dxf2Ruby.parse(ARGV.shift)
-  puts "Finsihed in #{Time.now - t1}"
-  dxf.keys.each do |section|
-    dxf[section].each do |line|
-      p line
+  pretty = ARGV.delete('-p')
+  filename = ARGV.shift
+  puts "Ruby Version: #{RUBY_VERSION}"
+  puts "File: #{File.expand_path(filename)}"
+  dxf = Dxf2Ruby.parse(filename)
+  puts "Finished in #{Time.now - t1}"
+  if pretty
+    require 'pp'
+    s = PP.pp(dxf, "")
+  else
+    s = ""
+    dxf.each do |sec, list|
+      s << sec.to_s << "\n"
+      list.each { |line| s << line.to_s << "\n" }
     end
   end
+  cmd = "less -S"
+  IO.popen(cmd, 'w') { |f| f.puts(s) }
 end
